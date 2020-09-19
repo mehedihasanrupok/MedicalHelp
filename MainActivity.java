228d500c44c8cd4bc1765fb84d7167de4d0bc03a
mehedihasanrupok <62742488+mehedihasanrupok@users.noreply.github.com>
2020-09-19 21:18:34 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.medicalhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login,register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Firebase connection success",Toast.LENGTH_LONG).show();
+
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
